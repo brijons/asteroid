bae21abbc8539e54464894016d6a27fce1fef6e0
bri <bri.mcconnell00@gmail.com>
2026-09-17 08:47:47 +0000

    asteroids

---
+++ main.py
@@ -0,0 +1,10 @@
+import pygame
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+
+print(f"Starting Asteroids with pygame version: {pygame.version.ver} {SCREEN_WIDTH} {SCREEN_HEIGHT}")
+
+def main():
+    print("Hello from asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
+if __name__ == "__main__":
+    main()
