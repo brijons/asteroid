4dfcff7dcc36e5d6dcd9fd065b8c8f0168e0357b
bri <bri.mcconnell00@gmail.com>
2026-09-18 17:53:43 +0000

    add player.py

---
+++ player.py
@@ -0,0 +1,27 @@
+from constants import PLAYER_RADIUS, LINE_WIDTH, TURN_SPEED
+from circleshape import CircleShape
+import pygame
+class Player(CircleShape):
+    def __init__(self, x: int, y: int) -> None:
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0.0
+    def rotate(self, dt: float) -> None:
+        self.rotation += TURN_SPEED * dt
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+    def move(self, dt: int) -> None:
+        TURN_SPEED * dt
